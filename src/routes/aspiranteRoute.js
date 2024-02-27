2f14f43d95de9ce82479f7b0ca854b7ee070c17d
Itzel <itzelfuentescabrera@gmail.com>
2024-02-27 17:03:15 -0600

    Primer commit

---
+++ src/routes/aspiranteRoute.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const pacienteController = require('../controllers/pacienteController.js');
+const router = express.Router();
+const multer = require('multer')
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
+
+
+
+// Crear paciente
+router.post('/pac', upload.fields([{ name: 'foto' }, { name: 'cert' }]), pacienteController.guardarPaciente);
+
+// Obtener todos los aspirantes
+router.get('/pac',pacienteController.obtenerPacientes);
+
+// Obtener todas las ciudades
+router.get('/pac/ciu',pacienteController.obtenerCiudades);
+
+// Obtener un aspirante
+router.get('/pac/:id',pacienteController.obtenerPaciente);
+
+module.exports = router;
